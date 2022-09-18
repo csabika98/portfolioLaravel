6575cccc6eb0f6764c2e0cedc2b347f5899961b9
csabika98 <6ix9inegotti@naver.com>
2022-09-18 18:31:12 +0200

    change content + db migration setup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <header class="header text-center">
         <h1 class="blog-name pt-lg-4 mb-0"><a class="no-text-decoration" href="{{url()->current()}}">Csaba Sallai</a></h1>
         <div class="bio mb-3"><br>Site information:<br><ul><li>Framework: Laravel v 9.28</li>
-                <li>Database: MySQL</li><li>PHP 8.1.10</li></ul></div>
+                <li>Database: MySQL</li><li>PHP 8.1.10</li><li>Boostrap 5 (theme created by Bootsrap 5)</li></ul></div>
         <div class="bio mb-3"><a class="btn btn-primary" href="{{ asset('cv/CVCsaba.pdf') }}" target="_blank" role="button">Download my CV</a></div>
         <nav class="navbar navbar-expand-lg navbar-dark" >
 
@@ -72,103 +72,12 @@
         </section>
 
 
-        <section class="blog-list px-3 py-5 p-md-5">
-            <div class="container single-col-max-width">
-                <div class="item mb-5">
-                    <div class="row g-3 g-xl-0">
-                        <div class="col-2 col-xl-3">
-                            <img class="img-fluid post-thumb " src="{{ asset('images/blog/blog-post-thumb-1.jpg') }}" alt="image">
-                        </div>
-                        <div class="col">
-                            <h3 class="title mb-1"><a class="text-link" href="blog-post.html">Top 3 JavaScript Frameworks</a></h3>
-                            <div class="meta mb-1"><span class="date">Published 2 days ago</span><span class="time">5 min read</span><span class="comment"><a class="text-link" href="#">8 comments</a></span></div>
-                            <div class="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies...</div>
-                            <a class="text-link" href="blog-post.html">Read more &rarr;</a>
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//item-->
-                <div class="item mb-5">
-                    <div class="row g-3 g-xl-0">
-                        <div class="col-2 col-xl-3">
-                            <img class="img-fluid post-thumb " src="{{ asset('images/blog/blog-post-thumb-2.jpg') }}" alt="image">
-                        </div>
-                        <div class="col">
-                            <h3 class="title mb-1"><a class="text-link" href="blog-post.html">About Remote Working</a></h3>
-                            <div class="meta mb-1"><span class="date">Published 3 months ago</span><span class="time">3 min read</span><span class="comment"><a class="text-link" href="#">26 comments</a></span></div>
-                            <div class="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies...</div>
-                            <a class="text-link" href="blog-post.html">Read more &rarr;</a>
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//item-->
-
-                <div class="item mb-5">
-                    <div class="row g-3 g-xl-0">
-                        <div class="col-2 col-xl-3">
-                            <img class="img-fluid post-thumb" src="{{ asset('images/blog/blog-post-thumb-3.jpg') }}" alt="image">
-                        </div>
-                        <div class="col">
-                            <h3 class="title mb-1"><a class="text-link" href="blog-post.html">A Guide to Becoming a Full-Stack Developer</a></h3>
-                            <div class="meta mb-1"><span class="date">Published 2 months ago</span><span class="time">15 min read</span><span class="comment"><a class="text-link" href="#">3 comments</a></span></div>
-                            <div class="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies...</div>
-                            <a class="text-link" href="blog-post.html">Read more &rarr;</a>
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//item-->
-
-                <div class="item mb-5">
-                    <div class="row g-3 g-xl-0">
-                        <div class="col-2 col-xl-3">
-                            <img class="img-fluid post-thumb" src="{{ asset('images/blog/blog-post-thumb-4.jpg') }}" alt="image">
-                        </div>
-                        <div class="col">
-                            <h3 class="title mb-1"><a class="text-link" href="blog-post.html">High Performance JavaScript</a></h3>
-                            <div class="meta mb-1"><span class="date">Published 1 month ago</span><span class="time">8 min read</span><span class="comment"><a class="text-link" href="#">12 comments</a></span></div>
-                            <div class="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies...</div>
-                            <a class="text-link" href="blog-post.html">Read more &rarr;</a>
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//item-->
+        <!--/* PAGE CONTENT */-->
 
 
-                <div class="item mb-5">
-                    <div class="row g-3 g-xl-0">
-                        <div class="col-2 col-xl-3">
-                            <img class="img-fluid post-thumb" src="{{ asset('images/blog/blog-post-thumb-5.jpg') }}" alt="image">
-                        </div>
-                        <div class="col">
-                            <h3 class="title mb-1"><a class="text-link" href="blog-post.html">Learn React in 24 Hours</a></h3>
-                            <div class="meta mb-1"><span class="date">Published 2 months ago</span><span class="time">10 min read</span><span class="comment"><a class="text-link" href="#">23 comments</a></span></div>
-                            <div class="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies...</div>
-                            <a class="text-link" href="blog-post.html">Read more &rarr;</a>
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//item-->
-
-                <div class="item">
-                    <div class="row g-3 g-xl-0">
-                        <div class="col-2 col-xl-3">
-                            <img class="img-fluid post-thumb" src="{{ asset('images/blog/blog-post-thumb-6.jpg') }}" alt="image">
-                        </div>
-                        <div class="col">
-                            <h3 class="title mb-1"><a class="text-link" href="blog-post.html">How to Build Desktop Apps with HTML, CSS and JavaScript</a></h3>
-                            <div class="meta mb-1"><span class="date">Published 3 months ago</span><span class="time">2 min read</span><span class="comment"><a class="text-link" href="#">1 comment</a></span></div>
-                            <div class="intro">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies...</div>
-                            <a class="text-link" href="blog-post.html">Read more &rarr;</a>
-                        </div><!--//col-->
-                    </div><!--//row-->
-                </div><!--//item-->
-
-                <nav class="blog-nav nav nav-justified my-5">
-                    <a class="nav-link-prev nav-item nav-link d-none rounded-left" href="#">Previous<i class="arrow-prev fas fa-long-arrow-alt-left"></i></a>
-                    <a class="nav-link-next nav-item nav-link rounded" href="#">Next<i class="arrow-next fas fa-long-arrow-alt-right"></i></a>
-                </nav>
-
-            </div>
-        </section>
 
         <footer class="footer text-center py-2 theme-bg-dark">
 
-            <!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
             <small class="copyright">Laravel v9.28</small>
 
         </footer>
