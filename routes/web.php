8828c5b282427c819e12c3783e23309c34a9d609
csabika98 <6ix9inegotti@naver.com>
2022-09-18 19:23:14 +0200

    Routes created

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::post("/contact", function (){
+   return view('contact');
+});
+
+Route::get("/about", function (){
+    return view('aboutme');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
